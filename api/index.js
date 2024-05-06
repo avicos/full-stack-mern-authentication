c80b4b15f3542ca34f7746d70c7ded315f747488
avicos <adapaavinash612@gmail.com>
2024-05-06 21:41:18 +0530

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -13,6 +15,8 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 const app =express();
 
+app.use(express.json());
+
 app.listen(3000, () =>{
     console.log('Server is listening on port 3000');
 });
@@ -20,4 +24,5 @@ app.listen(3000, () =>{
 
 
 app.use('/api/user', userRoutes);
+app.use("/api/auth", authRoutes);
 
