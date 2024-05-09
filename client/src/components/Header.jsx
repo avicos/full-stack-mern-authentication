90cc1b0a5e294ae0b9be3af8829fd2a31e55fcdd
avicos <adapaavinash612@gmail.com>
2024-05-09 14:29:05 +0530

    Update header component and makethe profile page private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom"
+import {useSelector} from 'react-redux';
 
 export default function Header() {
+  const {currentUser} = useSelector((state) => state.user);
+
   return (
     <div className='bg-slate-200'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -8,7 +11,16 @@ export default function Header() {
         <ul className='flex gap-4 '>
           <Link to='/'><l1>Home</l1></Link>
           <Link to='/about'><l1>About</l1></Link>
-          <Link to='/sign-in'><l1>Sign In</l1></Link>
+          <Link to='/profile'>
+            {currentUser ? (
+              <img 
+                src={currentUser.profilePicture} 
+                alt="profile"
+                className="h-7 w-7 rounded-full object-cover" />
+            ) : (
+            <l1>Sign In</l1>
+            )}
+          </Link>
           
           
           
